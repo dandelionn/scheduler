8f6240407f0390c22e94f7b717a050980996459e
DESKTOP-7BP57RP\Paul <michea.paul@yahoo.com>
2020-04-28 22:35:06 +0300

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [{
+      name: 'scheduler',
+      script: './backend/server.js'
+    }],
+    deploy: {
+      production: {
+        user: 'root',
+        host: '185.244.130.86',
+        key: './.ssh/vpskey.pem',
+        ref: 'origin/master',
+        repo: 'git@github.com:dandelionn/scheduler.git',
+        path: '/server/scheduler/',
+        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      }
+    }
+  }
